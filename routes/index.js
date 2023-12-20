abc7fda0dcfd185368827dc80e2eeb895ff0588c
SilverErokai1 <114986962+SilverErokai1@users.noreply.github.com>
2023-12-20 19:19:31 +0700

    Add Entity repair staus

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import bodypolygonroutes from './bodypolygonroutes.js'
 import meshobj from './meshobjectroute.js'
+import entityrepairstatusroutes from './entityrepairstatusroutes.js';
 function route(app){
     app.use('/bodypolygon', bodypolygonroutes);
     app.use('/mesh', meshobj);
+    app.use('/entityrepairstatus', entityrepairstatusroutes);
 }
 export default route;
