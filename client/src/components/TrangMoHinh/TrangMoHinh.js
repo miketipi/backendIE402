06f33375a77b8c7c984acc78188accb9d321290e
Bằng Phạm <bangbapham2002@gmail.com>
2023-12-21 19:04:15 +0700

    Test trang mô hình

---
+++ client/src/components/TrangMoHinh/TrangMoHinh.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TrangMoHinh from './MapComponent';
+export const TrangMoHinh = () =>  
+{
+  render() 
+  {
+    return (
+      <div>
+        <h1>Trang Mô Hình</h1>
+        <Map />
+      </div>
+    );
+  }
+}
