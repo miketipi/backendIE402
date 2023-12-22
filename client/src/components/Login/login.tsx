4ad23312102a7f1b882b16010a9aa83e7357b4ff
thaishoaf <20520510@gm.uit.edu.vn>
2023-12-22 16:06:08 +0700

    login

--- client/src/components/Login/login.tsx
+++ client/src/components/Login/login.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import axios from "axios";
 
 export const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post("http://localhost:4000/userControl/login", { username, password });
+            if (response.status === 200 && response.data.message === 'Login successful') {
+                console.log("Đăng nhập thành công");
+            } else {
+                console.error("Sai email hoặc mật khẩu");
+            }
+        } catch (error: any) { 
+            console.error("Error:", error.response?.data);
+        }
+    };
+
+
+
     return (
         <div className="index">
             <div className="div-3">
@@ -11,7 +30,7 @@ export const LoginForm = () => {
                         <a href="/register" className="text-wrapper-7"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Đăng ký</a>
                     </div>
                     <div className="div-row">
-                        <button className="button-thm-btn">
+                        <button className="button-thm-btn" onClick={handleLogin}>
                             <div className="overlap-group-2">
                                 <div className="div-4" />
                                 <div className="div-4" />
@@ -22,9 +41,9 @@ export const LoginForm = () => {
                             </div>
                         </button>
                         <div className="div-wrapper">
-                            <input className="input" placeholder="Mật khẩu" />
+                            <input className="input" placeholder="Mật khẩu" onChange={(e) => setPassword(e.target.value)} />
                         </div>
-                        <input className=" input" placeholder="Email" />
+                        <input className="input" placeholder="Email" onChange={(e) => setUsername(e.target.value)} />
                     </div>
                     <div className="paragraph-register-2">
                         <div className="text-wrapper-11">chỉ bằng một nút bấm</div>
@@ -33,7 +52,6 @@ export const LoginForm = () => {
                     <div className="text-wrapper-12">Đăng nhập</div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 };
-
