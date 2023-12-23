75062433e629a02c1c7831c5194bd273751693cc
miketipi <100526891+miketipi@users.noreply.github.com>
2023-12-23 22:45:08 +0700

    add user state

--- client/src/components/Login/login.tsx
+++ client/src/components/Login/login.tsx
@@ -1,18 +1,47 @@
-import React, { useState } from "react";
+import React, { createContext, useState, useContext, ReactNode, FunctionComponent } from 'react';
 import "./style.css";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
+interface User {
+    email: string;
+    password: string;
+    phone: string;
+    admin: boolean;
+    name: string;
+    _id: string
+}
+interface UserContextProps {
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+export const UserContext = createContext<UserContextProps>({
+    user: null,
+    setUser: (value: React.SetStateAction<User | null>) => { } // Đây là hàm với một tham số
+});
+
+interface UserProviderProps {
+    children: ReactNode;
+}
+export const UserProvider: FunctionComponent<UserProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
 
 export const LoginForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-
+    const { setUser } = useContext(UserContext);
     const handleLogin = async () => {
         try {
-            const response = await axios.post<{ message: string }>(
+            const response = await axios.post<{ message: string, user: User }>(
                 "http://localhost:4000/userControl/login",
                 { email, password }
             );
@@ -21,6 +50,8 @@ export const LoginForm = () => {
                 console.log("Đăng nhập thành công");
                 alert("Đăng nhập thành công!");
                 navigate('/');
+                setUser(response.data.user);
+                console.log(response.data.user);
             } else {
                 console.error("Sai email hoặc mật khẩu");
                 alert("Đăng nhập không thành công! Vui lòng kiểm tra lại email và mật khẩu!");
