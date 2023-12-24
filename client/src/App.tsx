4bf92545aefb182cc52b94225bb2a4425bc07c09
tunglam241 <20521521@gm.uit.edu.vn>
2023-12-24 16:12:48 +0700

    add page repari_report

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
-import { memo } from 'react';
-import type { FC } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import classes from './App.module.css';
-import resets from './components/_resets.module.css';
-import { TrangChu } from './components/TrangChu/TrangChu';
-import { LoginForm } from './components/Login/login';
-import { RegisterForm } from './components/Register/register';
+import { memo } from "react";
+import type { FC } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import classes from "./App.module.css";
+import resets from "./components/_resets.module.css";
+import { TrangChu } from "./components/TrangChu/TrangChu";
+import { LoginForm } from "./components/Login/login";
+import { RegisterForm } from "./components/Register/register";
+import Repair_report from "./components/Repair_Report/repair_report";
 interface Props {
   className?: string;
 }
@@ -18,9 +19,10 @@ export const App: FC<Props> = memo(function App(props = {}) {
           <Route path="/" element={<TrangChu />} />
           <Route path="/about" element={<TrangChu />} />
           <Route path="/contact" element={<TrangChu />} />
-          <Route path="/model" element = {<TrangChu/>}/>
+          <Route path="/model" element={<TrangChu />} />
           <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm/>}/>
+          <Route path="/repair-report" element={<Repair_report />} />
+          <Route path="/register" element={<RegisterForm />} />
         </Routes>
       </Router>
     </div>
